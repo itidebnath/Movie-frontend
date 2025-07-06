f41877b9fad780a9ec95f8ba3e4573a9b98e78e9
Iti Debnath <itiidebnath60@gmail.com>
2025-07-06 09:22:30 +0530

    Update: Made changes

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,7 @@
 // src/services/movieService.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api'; // adjust if different
+const API_BASE = 'https://movie-site-0zcr.onrender.com/api'; // adjust if different
 
 export const getAllMovies = async () => {
   const { data } = await axios.get(`${API_BASE}/movies`);
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://movie-site-0zcr.onrender.com/api",
 });
 
 // ✅ Add token automatically to headers
